9a4d93090b3431d5fd24a9a3dc6cb6a78c706f1d
Zero Liu <zero@fireworkhq.com>
2022-01-13 14:51:24 -0800

    Update extension manifest

--- golink-extension/background.js
+++ golink-extension/background.js
@@ -1,6 +1,6 @@
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener((text) => {
   // Encode user input for special characters , / ? : @ & = + $ #
-  const newURL = 'http://go/' + encodeURIComponent(text);
-  chrome.tabs.create({ url: newURL });
+  var newURL = 'http://go/' + encodeURIComponent(text);
+  chrome.tabs.update({ url: newURL });
 });
---
+++ golink-web/pages/api/go/index.js
@@ -0,0 +1,4 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default async function handler(req, res) {
+  res.redirect('/');
+}
